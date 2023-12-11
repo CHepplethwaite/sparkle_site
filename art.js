0d915e4348bc17236e4d0cf0e4d0a6f1f63ed87e
Treasure Chinyimba <Treasurearthur4@gmail.com>
2023-12-11 10:28:28 +0200

    added a scroll up button when a user goes down and wants to scroll up

--- art.js
+++ art.js
@@ -1,3 +1,11 @@
 function hideInfo() {
     document.getElementById('infoContainer').style.display = 'none';
 }
+
+//function for scrolling to the top
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
