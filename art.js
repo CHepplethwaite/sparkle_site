cccd4904c76a4d9c44e60024134b994b5bd9b2fa
Treasure Chinyimba <Treasurearthur4@gmail.com>
2023-12-11 10:52:18 +0200

    Removed the duplicate scroll up

--- art.js
+++ art.js
@@ -1,11 +1,3 @@
 function hideInfo() {
     document.getElementById('infoContainer').style.display = 'none';
 }
-
-//function for scrolling to the top
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-}
